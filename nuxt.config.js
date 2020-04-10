b468f0f4d69723a33f10069e89132c10de7da95d
John Wanyoike <jhnwanyoike@gmail.com>
2020-04-10 20:41:34 +0300

    minor

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,6 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
       {name: 'author', content: 'John Wanyoike'},
-      { 'http-equiv': 'refresh', content: '60' },
       {name: 'title', content: 'ContiPartner Your Fitment Professionals'},
       
       {property: 'og:title', content: 'Silverstone | Silverstone ContiPartner' },
